4c4cf6a07a15cd6df21cc9e271ea993a622de6bf
sihyun <sihyun615@naver.com>
2024-04-28 17:08:11 +0900

    1-1) 양의 정수 2개 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,16 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();  //입력 정수 저장
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();  //입력 정수 저장
 
     }
 }
