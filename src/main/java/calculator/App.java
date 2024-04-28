eadf2e329354c77b4ce00746a790c903fa00ee6b
sihyun <sihyun615@naver.com>
2024-04-28 17:32:49 +0900

    1-3) 연산하여 결과값 출력하기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -16,5 +16,33 @@ public class App {
         //next()의 반환값이 string이므로 charAt(0)으로 char로 바꾼후 0번째 char 반환
         char operator = sc.next().charAt(0);
 
+        int result = 0;
+        switch (operator) {  //operator의 종류 : +,-,*,/
+            case '+' :
+                result = num1 + num2;
+                break;
+            case '-' :
+                result = num1 - num2;
+                break;
+            case '*' :
+                result = num1 * num2;
+                break;
+            case '/' :
+                if (num2 == 0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
+                else {
+                    result = num1 / num2;
+                }
+                break;
+            default :
+                System.out.println("잘못된 연산자입니다.");
+                return;
+        }
+
+        if(operator!='/' || num2!=0){  //나눗셈 연산에서 분모(두번째 정수)가 0인 경우 result 출력X!
+            System.out.println("결과: " + result);
+        }
+
     }
 }
