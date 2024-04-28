c9bcd0891b32749f44cbe0b588d1a0ec46526c39
sihyun <sihyun615@naver.com>
2024-04-28 17:13:25 +0900

    1-2) 사칙연산 기호 입력받기

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -12,5 +12,9 @@ public class App {
         System.out.print("두 번째 숫자를 입력하세요: ");
         int num2 = sc.nextInt();  //입력 정수 저장
 
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        //next()의 반환값이 string이므로 charAt(0)으로 char로 바꾼후 0번째 char 반환
+        char operator = sc.next().charAt(0);
+
     }
 }
