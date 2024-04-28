56a7c67e66b147c87ac6c532df06d0c69755bcdd
sihyun <sihyun615@naver.com>
2024-04-28 19:11:59 +0900

    1-8) "inquiry"입력 시 저장된 연산결과 전부 출력

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,14 +1,15 @@
 package calculator;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        List<Integer> results = new ArrayList<>(); // 연산 결과들을 저장할 리스트
+
+        // ArrayList : 동적 배열이라 크기를 지정하지 않아도 됨
+        ArrayList<Integer> results = new ArrayList<>(); // 연산 결과들을 저장할 리스트
 
         Scanner sc = new Scanner(System.in);
 
@@ -22,6 +23,7 @@ public class App {
             System.out.print("사칙연산 기호를 입력하세요: ");
             //next()의 반환값이 string이므로 charAt(0)으로 char로 바꾼후 0번째 char 반환
             char operator = sc.next().charAt(0);
+
 
 
             int result = 0;
@@ -48,8 +50,8 @@ public class App {
                     continue; // 다음 반복으로 넘어감
             }
 
-            System.out.println("결과: " + result);
 
+            System.out.println("결과: " + result);
             results.add(result);  //results리스트에 연산결과 추가
 
 
@@ -64,12 +66,19 @@ public class App {
             }
 
 
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+            if (Objects.equals(sc.next(), "inquiry")){  //입력받은 값이 "inquiry"이면
+                for (int value : results) { // 저장된 연산결과들 출력
+                    System.out.println(value);
+                }
+            }
+
+
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             if (Objects.equals(sc.next(), "exit")){  //입력받은 값이 "exit"이면 while문 종료
                 break;
             }
         }
-
 
     }
 }
